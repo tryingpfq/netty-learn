921f0f2ab07f3a2a866c11665cfe308c6a1277ec
tryingpfq <tryingpfq@163.com>
2018-08-25 11:23:14 +0800

    客户端逻辑处理

--- nettyparent/client/src/main/java/client/EchoClientHandler.java
+++ nettyparent/client/src/main/java/client/EchoClientHandler.java
@@ -1,8 +1,10 @@
 package client;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
 
 /**
  *客户端逻辑
@@ -10,10 +12,19 @@ import io.netty.channel.SimpleChannelInboundHandler;
 public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        super.channelActive(ctx);
+        //当前通知Channel是活跃的时候，发送一条信息
+        ctx.writeAndFlush(Unpooled.copiedBuffer("netty rocks!",CharsetUtil.UTF_8));
     }
 
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+        //记录已接受消息的转储
+        System.out.println("Client received :" +
+            byteBuf.toString(CharsetUtil.UTF_8));
+    }
 
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
     }
 }
